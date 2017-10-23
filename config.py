0f94550b2ddc9bc8d3fa3a673fad6473013fe434
Nicole Nguyen <nicole.nguyen321@gmail.com>
2017-10-23 21:03:23 +0000

    added 25 entries to blogful c9

---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+class DevelopmentConfig(object):
+    SQLALCHEMY_DATABASE_URI = "postgresql://nicole:thinkful@localhost:5432/blogful"
+    DEBUG = True
---
+++ views.py
@@ -0,0 +1,18 @@
+from flask import request, Response
+from flask import render_template
+
+from . import app
+from .database import session, Entry
+
+@app.route("/")
+# def index():
+#     data = 10
+#     return Response (data, mimetype="application.json")
+
+
+@app.route("/")
+def entries():
+    entries = session.query(Entry)
+    entries = entries.order_by(Entry.datetime.desc())
+    entries = entries.all()
+    return render_template("entries.html", entries=entries)
